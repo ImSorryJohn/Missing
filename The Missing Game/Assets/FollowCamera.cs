48dd6a09946dac99b3c075927e92f5f716187790
Starlitriver1234 <C24745999@mytudublin.ie>
2025-04-05 21:41:39 +0100

    improved the script

--- The Missing Game/Assets/FollowCamera.cs
+++ The Missing Game/Assets/FollowCamera.cs
@@ -3,14 +3,13 @@ using UnityEngine;
 public class FollowCamera : MonoBehaviour
 {
     public float FollowSpeed = 2f;
-    public float yoffset = 1f;
     public Transform target;
     
    
 
     void Update()
     {
-        Vector3 newPos = new Vector3(target.position.x, target.position.y + yoffset, -10f);
+        Vector3 newPos = new Vector3(target.position.x, target.position.y, -10f);
         transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
     }
 }
